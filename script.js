2344ef68918a5723c345987e351ff2c1dc1d968c
Joyce Fernandes <joyce_f.s@hotmail.com>
2022-09-21 21:27:36 -0300

    adicionando a lógica

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const board = document.querySelector("[data-board]");
 
 let isCircleTurn;
 
+const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
 const startGame = () => {
     for (const cell of cellElements) {
         cell.addEventListener("click", handleClick, { once: true });
@@ -12,6 +23,14 @@ const startGame = () => {
 
     board.classList.add("x");
 };
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some((combination) => {
+      return combination.every((index) => {
+        return cellElements[index].classList.contains(currentPlayer);
+      });
+    });
+  };
 
 const placeMark = (cell, classToAdd) => {
     cell.classList.add(classToAdd);
@@ -29,11 +48,11 @@ const swapTurns = () => {
         board.classList.add("x");
     }
 };
- 
+
 const handleClick = (e) => {
     const cell = e.target;
     const classToAdd = isCircleTurn ? "circle" : "x";
-    
+
     placeMark(cell, classToAdd)
 
     swapTurns();
