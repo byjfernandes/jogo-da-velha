132ce38a69e6d98b1c0b1e86a8acd270ae8f797a
Joyce Fernandes <joyce_f.s@hotmail.com>
2022-09-21 21:16:59 -0300

    criação da lógica

---
+++ script.js
@@ -0,0 +1,43 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+
+let isCircleTurn;
+
+const startGame = () => {
+    for (const cell of cellElements) {
+        cell.addEventListener("click", handleClick, { once: true });
+    }
+
+    isCircleTurn = false;
+
+    board.classList.add("x");
+};
+
+const placeMark = (cell, classToAdd) => {
+    cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+    isCircleTurn = !isCircleTurn;
+
+    board.classList.remove("circle");
+    board.classList.remove("x");
+
+    if (isCircleTurn) {
+        board.classList.add("circle");
+    } else {
+        board.classList.add("x");
+    }
+};
+ 
+const handleClick = (e) => {
+    const cell = e.target;
+    const classToAdd = isCircleTurn ? "circle" : "x";
+    
+    placeMark(cell, classToAdd)
+
+    swapTurns();
+};
+
+startGame();
+
